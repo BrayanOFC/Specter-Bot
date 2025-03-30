7e401e717d402227d5bc200f9eb4ffa75b8c243c
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-30 14:13:37 -0600

    Crear plugins/dl-youtube_video.js

---
+++ plugins/dl-youtube_video.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+import fg from 'senna-fg';
+
+let handler = async(m, { conn, args, text }) => {
+
+if (!text) return m.reply(`★ Ingresa Un Link De YouTube`);
+
+m.react(rwait);
+
+let data = await fg.ytmp4(text);
+let url = data.dl_url;
+
+if (!url) return m.reply('《✧》Hubo un error al intentar acceder al link.\n> Si el problema persiste, reportalo en el grupo de soporte.');
+
+await conn.sendMessage(m.chat, {
+      video: { url: url },
+      mimetype: "video/mp4",
+      caption: `${dev}`,
+    }, { quoted: m });
+    m.react(done);
+ }
+
+handler.command = ['ytmp4', 'mp4']
+
+export default handler;
