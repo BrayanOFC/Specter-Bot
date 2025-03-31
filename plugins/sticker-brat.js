f13a813a2d27f6d87c028783a596d4a153ac12b0
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-30 20:22:24 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,79 +1,17 @@
+import { sticker } from '../lib/sticker.js'
+import fetch from 'node-fetch'
 import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import sharp from 'sharp';
-import {
-    tmpdir
-} from 'os';
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const fetchSticker = async (text, attempt = 1) => {
-    try {
-        const response = await axios.get(`https://kepolu-brat.hf.space/brat`, {
-            params: {
-                q: text
-            },
-            responseType: 'arraybuffer',
-        });
-        return response.data;
-    } catch (error) {
-        if (error.response?.status === 429 && attempt <= 3) {
-            const retryAfter = error.response.headers['retry-after'] || 5;
-            await delay(retryAfter * 1000);
-            return fetchSticker(text, attempt + 1);
-        }
-        throw error;
-    }
-};
+let handler = async(m, { conn, text, args, usedPrefix, command }) => {
+    if (!text) return m.reply(`《★》Ingresa Un Texto Para Realizar Tu Sticker\n> *Ejemplo:* ${usedPrefix + command} CrowBot`);
 
-const handler = async (m, {
-    text,
-    conn
-}) => {
-    if (!text) {
-        return conn.sendMessage(m.chat, {
-            text: '🍬 Por favor ingresa el texto para hacer un sticker.',
-        }, {
-            quoted: m
-        });
-    }
+let stiker = await sticker(null,`https://api.fgmods.xyz/api/maker/carbon?text=${text}&apikey=elrebelde21`,global.packname, global.author)
+conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: dev, mediaType: 2, sourceUrl: redes, thumbnail: imagen1 }}}, { quoted: m })
+}
 
-    try {
-        const buffer = await fetchSticker(text);
-        const outputFilePath = path.join(tmpdir(), `sticker-${Date.now()}.webp`);
-        await sharp(buffer)
-            .resize(512, 512, {
-                fit: 'contain',
-                background: {
-                    r: 255,
-                    g: 255,
-                    b: 255,
-                    alpha: 0
-                }
-            })
-            .webp({
-                quality: 80
-            })
-            .toFile(outputFilePath);
-
-        await conn.sendMessage(m.chat, {
-            sticker: {
-                url: outputFilePath
-            },
-        }, {
-            quoted: fkontak
-        });
-        fs.unlinkSync(outputFilePath);
-    } catch (error) {
-        return conn.sendMessage(m.chat, {
-            text: `⚠️ Ocurrio un erro.`,
-        }, {
-            quoted: m
-        });
-    }
-};
+handler.help = ['brat'];
+handler.tag = ['sticker'];
 handler.command = ['brat'];
-handler.tags = ['sticker'];
-handler.help = ['brat *<texto>*'];
+handler.estrellas = 3;
 
 export default handler;
